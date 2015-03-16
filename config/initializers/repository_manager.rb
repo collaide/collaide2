a1bc3cd3a9f975985a43b8d3f6ba8a8741e49db9
facenord <facenord.sud@gmail.com>
2015-03-16 16:18:35 +0100

    test

--- config/initializers/repository_manager.rb
+++ config/initializers/repository_manager.rb
@@ -29,12 +29,13 @@ if Rails.env.production?
         aws_access_key_id:     ENV['S3_KEY'],                        # required
         aws_secret_access_key: ENV['S3_ACCESS'],                        # required
         region:                'us-west-2',                  # optional, defaults to 'us-east-1'
-        path_style: true
+        #path_style: true
         # host:                  's3.example.com',             # optional, defaults to nil
         # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
     }
     config.fog_directory  = ENV['S3_BUCKET']                          # required
-    config.fog_public     = false                                        # optional, defaults to true
-    #config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+    config.fog_public     = false
+    config.fog_authenticated_url_expiration = 7.days.to_i# optional, defaults to true
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
   end
 end
