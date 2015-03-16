924675ad1685aa0d75407ffb8ebd55386f57eba2
facenord <facenord.sud@gmail.com>
2015-03-16 16:51:50 +0100

    mieux?

--- config/initializers/repository_manager.rb
+++ config/initializers/repository_manager.rb
@@ -27,7 +27,7 @@ if Rails.env.production?
     config.storage    = :aws
     config.aws_bucket = ENV.fetch('S3_BUCKET')
     config.aws_acl    = :public_read
-    config.asset_host = 'http://beta.collaide.com'
+    # config.asset_host = 'http://beta.collaide.com'
     config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
     config.aws_credentials = {
